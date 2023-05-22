155803f1e3660d4f030c44a4da60188f72293e6c
Constantine Kosyanov <12459928+ConstKosyanov@users.noreply.github.com>
2023-05-22 16:43:21 +0300

    add Autofac

--- RndTech.Talks.WebApplicatoinFactory/HomeController.cs
+++ RndTech.Talks.WebApplicatoinFactory/HomeController.cs
@@ -6,6 +6,10 @@ namespace RndTech.Talks.WebApplicatoinFactory;
 [Route("")]
 public class HomeController : Controller
 {
+	private readonly string pingResponse;
+
+	public HomeController(string pingResponse) => this.pingResponse = pingResponse;
+
 	[HttpGet("ping")]
-	public string Ping() => "working";
+	public string Ping() => pingResponse;
 }
--- RndTech.Talks.WebApplicatoinFactory/Program.cs
+++ RndTech.Talks.WebApplicatoinFactory/Program.cs
@@ -1,7 +1,11 @@
+using Autofac;
+using Autofac.Extensions.DependencyInjection;
+
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
+builder.Host.UseServiceProviderFactory(new AutofacServiceProviderFactory())
+	.ConfigureContainer<ContainerBuilder>(b => b.RegisterInstance("working"));
 
 var app = builder.Build();
-app.UseAuthorization();
 app.MapControllers();
 app.Run();
