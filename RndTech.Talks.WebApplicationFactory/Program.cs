5efb35b0acdeb7ee2544f3e9f3a9cc23f61bd61c
Constantine Kosyanov <12459928+ConstKosyanov@users.noreply.github.com>
2023-05-27 22:32:18 +0300

    reject top-level statement

--- RndTech.Talks.WebApplicationFactory/Program.cs
+++ RndTech.Talks.WebApplicationFactory/Program.cs
@@ -1,11 +1,19 @@
 using Autofac;
 using Autofac.Extensions.DependencyInjection;
 
-var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddControllers();
-builder.Host.UseServiceProviderFactory(new AutofacServiceProviderFactory())
-	.ConfigureContainer<ContainerBuilder>(b => b.RegisterInstance("working"));
+namespace RndTech.Talks.WebApplicationFactory;
 
-var app = builder.Build();
-app.MapControllers();
-app.Run();
+public class Program
+{
+	private static void Main(string[] args)
+	{
+		var builder = WebApplication.CreateBuilder(args);
+		builder.Services.AddControllers();
+		builder.Host.UseServiceProviderFactory(new AutofacServiceProviderFactory())
+			.ConfigureContainer<ContainerBuilder>(b => b.RegisterInstance("working"));
+
+		var app = builder.Build();
+		app.MapControllers();
+		app.Run();
+	}
+}
