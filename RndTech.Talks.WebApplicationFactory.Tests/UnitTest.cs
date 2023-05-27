82ba1221040499a87d9544cce544ff86796b7aa1
Constantine Kosyanov <12459928+ConstKosyanov@users.noreply.github.com>
2023-05-27 22:36:34 +0300

    implement PingTest

--- RndTech.Talks.WebApplicationFactory.Tests/UnitTest.cs
+++ RndTech.Talks.WebApplicationFactory.Tests/UnitTest.cs
@@ -1,10 +1,21 @@
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.VisualStudio.TestPlatform.TestHost;
+
 namespace RndTech.Talks.WebApplicationFactory.Tests;
 
-public class UnitTest
+public class UnitTest : IClassFixture<WebApplicationFactory<Program>>
 {
+	private readonly WebApplicationFactory<Program> webApplicationFactory;
+
+	public UnitTest(WebApplicationFactory<Program> webApplicationFactory)
+	{
+		this.webApplicationFactory = webApplicationFactory;
+	}
+
 	[Fact]
 	public async Task PingTest()
 	{
-
+		var httpClient = webApplicationFactory.CreateClient();
+		Assert.Equal("working", await httpClient.GetStringAsync("ping"));
 	}
 }
