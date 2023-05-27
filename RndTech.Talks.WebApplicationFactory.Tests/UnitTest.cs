e048edde48d0e8b5ef6a59c2afafa00d3af566af
Constantine Kosyanov <12459928+ConstKosyanov@users.noreply.github.com>
2023-05-27 22:42:55 +0300

    implement PingTest

--- RndTech.Talks.WebApplicationFactory.Tests/UnitTest.cs
+++ RndTech.Talks.WebApplicationFactory.Tests/UnitTest.cs
@@ -1,10 +1,32 @@
+using Autofac;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Extensions.Hosting;
+using Microsoft.VisualStudio.TestPlatform.TestHost;
+
 namespace RndTech.Talks.WebApplicationFactory.Tests;
 
-public class UnitTest
+public class UnitTest : IClassFixture<MyWebApplicationFactory>
 {
+	private readonly MyWebApplicationFactory webApplicationFactory;
+
+	public UnitTest(MyWebApplicationFactory webApplicationFactory)
+	{
+		this.webApplicationFactory = webApplicationFactory;
+	}
+
 	[Fact]
 	public async Task PingTest()
 	{
+		var httpClient = webApplicationFactory.CreateClient();
+		Assert.Equal("inherited", await httpClient.GetStringAsync("ping"));
+	}
+}
 
+public class MyWebApplicationFactory : WebApplicationFactory<Program>
+{
+	protected override IHost CreateHost(IHostBuilder builder)
+	{
+		builder.ConfigureContainer<ContainerBuilder>(b => b.RegisterInstance("inherited"));
+		return base.CreateHost(builder);
 	}
 }
