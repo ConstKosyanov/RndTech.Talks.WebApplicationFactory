21be0f33a07d3978afd152037f04bffcb669f96f
Constantine Kosyanov <12459928+ConstKosyanov@users.noreply.github.com>
2023-05-27 22:46:08 +0300

    implement PingTest

--- RndTech.Talks.WebApplicationFactory.Tests/UnitTest.cs
+++ RndTech.Talks.WebApplicationFactory.Tests/UnitTest.cs
@@ -1,10 +1,35 @@
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.VisualStudio.TestPlatform.TestHost;
+
 namespace RndTech.Talks.WebApplicationFactory.Tests;
 
-public class UnitTest
+public class UnitTest : IClassFixture<CompositeWebApplicationFactory>
 {
-	[Fact]
-	public async Task PingTest()
-	{
+	private readonly CompositeWebApplicationFactory composite;
 
+	public UnitTest(CompositeWebApplicationFactory composite) => this.composite = composite;
+
+	[Fact]
+	public async Task PingTest() => Assert.All(await composite.GetStringAsync("ping"), x => Assert.Equal("working", x));
+}
+
+public class CompositeWebApplicationFactory : IAsyncDisposable
+{
+	public WebApplicationFactory<Program> WebApplicationFactory1 { get; } = new WebApplicationFactory<Program>();
+	public WebApplicationFactory<Program> WebApplicationFactory2 { get; } = new WebApplicationFactory<Program>();
+
+	public async Task<IEnumerable<string>> GetStringAsync(string requestUrl)
+	{
+		return await Task.WhenAll(new[]
+		{
+			WebApplicationFactory1.CreateClient().GetStringAsync(requestUrl),
+			WebApplicationFactory2.CreateClient().GetStringAsync(requestUrl)
+		});
+	}
+
+	public async ValueTask DisposeAsync()
+	{
+		await WebApplicationFactory1.DisposeAsync();
+		await WebApplicationFactory2.DisposeAsync();
 	}
 }
