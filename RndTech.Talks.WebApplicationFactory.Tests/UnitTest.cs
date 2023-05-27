83a5aa7d7ffe0e5030dc7d3ab6c4d3a0bf8d44fe
Constantine Kosyanov <12459928+ConstKosyanov@users.noreply.github.com>
2023-05-27 22:02:06 +0300

    implement PingTest

--- RndTech.Talks.WebApplicationFactory.Tests/UnitTest.cs
+++ RndTech.Talks.WebApplicationFactory.Tests/UnitTest.cs
@@ -5,6 +5,7 @@ public class UnitTest
 	[Fact]
 	public async Task PingTest()
 	{
-
+		var httpClient = new HttpClient();
+		Assert.Equal("working", await httpClient.GetStringAsync("http://localhost:5000/ping"));
 	}
 }
