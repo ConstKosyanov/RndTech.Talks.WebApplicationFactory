2b593d3d0e6c07a55ec67a3f9314f804f1e5c572
Constantine Kosyanov <12459928+ConstKosyanov@users.noreply.github.com>
2023-05-28 22:50:46 +0300

    implement  PingTest

--- RndTech.Talks.WebApplicationFactory.Tests/UnitTest.cs
+++ RndTech.Talks.WebApplicationFactory.Tests/UnitTest.cs
@@ -1,10 +1,42 @@
+using System.Reflection;
+using Autofac;
+using Autofac.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Mvc.ApplicationParts;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
 namespace RndTech.Talks.WebApplicationFactory.Tests;
 
-public class UnitTest
+public class UnitTest : IClassFixture<HostFixture>
 {
 	[Fact]
 	public async Task PingTest()
 	{
-
+		var client = new HttpClient();
+		Assert.Equal("working", await client.GetStringAsync("http://localhost:5000/ping"));
 	}
+}
+
+public class HostFixture : IAsyncDisposable
+{
+	private readonly WebApplication app;
+
+	public HostFixture()
+	{
+		var builder = WebApplication.CreateBuilder();
+		builder.Services.AddControllers()
+			.PartManager
+			.ApplicationParts
+			.Add(new AssemblyPart(Assembly.Load("RndTech.Talks.WebApplicationFactory")));
+
+		builder.Host.UseServiceProviderFactory(new AutofacServiceProviderFactory())
+			.ConfigureContainer<ContainerBuilder>(b => b.RegisterInstance("working"));
+
+		app = builder.Build();
+		app.MapControllers();
+		app.Start();
+	}
+
+	public ValueTask DisposeAsync() => app.DisposeAsync();
 }
